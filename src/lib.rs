8be85b69b2c9678f5888beb5ac1731724dad0a53
Hasono <2105689721@qq.com>
2025-07-21 21:26:02 +0800

    feat: 支持大小写敏感搜索

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+use core::str;
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -13,14 +16,53 @@ impl Config {
         }
         let query: String = args[1].clone();
         let file_path: String = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.file_path)?;
-    println!("With contents: \n{}", contents);
+
+    let results: Vec<&str> = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let lowercase_query: String = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&lowercase_query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // 对 build 返回的 `Result` 进行处理
     let config: Config = Config::build(&args).unwrap_or_else(|err: &str| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,7 +15,7 @@ fn main() {
     println!("In file: {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
